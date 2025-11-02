802025305613c032a116c968526c07005f9a108a
Akil Tajwar <akiltajwar@gmail.com>
2025-11-02 16:39:23 +0600

    feat: remaining amount api

--- src/routes/dashboard.routes.ts
+++ src/routes/dashboard.routes.ts
@@ -1,9 +1,10 @@
 import express from 'express'
-import { getItemSummaryController } from '../controllers/dashboard.controller'
+import { getItemSummaryController, getRemainingAmountController } from '../controllers/dashboard.controller'
 import { authenticateUser } from '../middlewares/auth.middleware'
 
 const router = express.Router()
 
 router.get('/item-summary', authenticateUser, getItemSummaryController)
+router.get('/remaining-amount', authenticateUser, getRemainingAmountController)
 
 export default router
